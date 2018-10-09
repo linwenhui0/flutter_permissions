52b3b4750a449989bf83200855ff60e31786ae86
linwenhui <1252908234@qq.com>
2018-10-09 22:50:11 +0800

    +）创建项目

---
+++ flutter_permissions/ios/Classes/FlutterPermissionsPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterPermissionsPlugin : NSObject<FlutterPlugin>
+@end
---
+++ flutter_permissions/ios/Classes/FlutterPermissionsPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterPermissionsPlugin.h"
+
+@implementation FlutterPermissionsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_permissions"
+            binaryMessenger:[registrar messenger]];
+  FlutterPermissionsPlugin* instance = [[FlutterPermissionsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
